ca523235771d7c2fd10bf0a5df0368aabf4ee074
Master0r0 <reece191@msn.com>
2017-07-30 03:39:55 +0100

    Add arguement check

--- outputFunctions.lua
+++ outputFunctions.lua
@@ -2,12 +2,17 @@ local comp = require("component")
 local fs = require("filesystem")
 
 component = ...
-
-local dir = "home/functions/"..component..".txt"
-writeStream, err = fs.open(dir,"wb")
-print(err)
-for i,k in pairs(comp[component]) do
-    outString = string.format("%s():%s\n",i,k)
-    writeStream:write(outString)
+if component then
+    local dir = "home/functions/"..component..".txt"
+    writeStream, err = fs.open(dir,"wb")
+    if err then
+        print(err)
+    end
+    for i,k in pairs(comp[component]) do
+        outString = string.format("%s():%s\n",i,k)
+        writeStream:write(outString)
+    end
+    writeStream:close()
+else
+    print("Usage: outputFunctions.lua componentName")
 end
-writeStream:close()
