4d9edadca5ac8ede6e9bb299174ae067d60d9f79
Master0r0 <Master0r0@me.com>
2017-07-30 03:51:38 +0100

    Create Functions folder if doesn't exist

--- outputFunctions.lua
+++ outputFunctions.lua
@@ -1,18 +1,26 @@
 local comp = require("component")
 local fs = require("filesystem")
+local shell = require("shell")
+
+folderLoc = shell.getWorkingDirectory()
 
 component = ...
+
+if not fs.isDirectory(folderLoc.."/functions") then
+    fs.makeDirectory(folderLoc.."/functions")
+end
 if component then
-    local dir = "home/functions/"..component..".txt"
+    local dir = folderLoc.."/functions/"..component..".txt"
     writeStream, err = fs.open(dir,"wb")
     if err then
         print(err)
+    else
+        for i,k in pairs(comp[component]) do
+            outString = string.format("%s():%s\n",i,k)
+            writeStream:write(outString)
+        end
+        writeStream:close()
     end
-    for i,k in pairs(comp[component]) do
-        outString = string.format("%s():%s\n",i,k)
-        writeStream:write(outString)
-    end
-    writeStream:close()
 else
     print("Usage: outputFunctions.lua componentName")
 end
